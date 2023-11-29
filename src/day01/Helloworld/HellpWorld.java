8f432534490d47e3a7d90d7969d48e5320a8b8dd
조수현 <jeniffer0812@naver.com>
2023-11-29 21:14:10 +0900

    .

--- src/day01/Helloworld/HellpWorld.java
+++
@@ -1,8 +0,0 @@
-package day01.Helloworld;
-
-public class HellpWorld {
-    public static void main(String[] args) {
-    	System.out.println("Hello Java");
-        System.out.println("Hello world");
-    }
-}
